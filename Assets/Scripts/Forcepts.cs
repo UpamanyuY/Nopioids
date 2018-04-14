6793f1c51ecb8961f7d3492ddc44941df73c6811
DESKTOP-HQJ4Q4R\Savion Thomas <savion1300@gmail.com>
2018-04-14 15:45:37 -0400

    Scripts
    Forcepts and movement script for the untensils

---
+++ Assets/Scripts/Forcepts.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcepts : MonoBehaviour {
+
+	public bool grabbed;
+	public GameObject grabobject;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(grabbed)
+		{
+			grabobject.transform.position = new Vector3 (transform.position.x, transform.position.y - .5f , transform.position.z);
+
+			if(Input.GetButtonDown ("Fire1"))
+			{
+				grabobject.GetComponent<Rigidbody> ().useGravity = true;
+				grabobject.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+				grabobject = null;
+				grabbed = false;
+			}
+		}
+
+
+			
+	}
+
+	void OnTriggerEnter (Collider other )
+	{
+		if (other.gameObject.tag == "Gunk")
+		{
+			grabobject = other.gameObject;
+			other.GetComponent <Rigidbody> ().useGravity = false;
+			other.GetComponent <Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			grabbed = true;
+		}
+	}
+
+	void OnTriggerExit ()
+	{
+		
+	}
+
+	void OnTriggerStay()
+	{
+		
+	}
+
+
+}
---
+++ Assets/Scripts/UtensilMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtensilMovement : MonoBehaviour {
+
+	public bool Disabled;
+
+	public float HSpeed;
+
+	public float ZSpeed;
+
+	public float YSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!Disabled)
+		{
+			var x = Input.GetAxis ("Horizontal") * Time.deltaTime * HSpeed;
+
+			var z = Input.GetAxis ("Vertical") * Time.deltaTime * ZSpeed;
+
+			var y = Input.GetAxis ("Vertical2") * Time.deltaTime * YSpeed;
+
+			transform.Rotate (z, 0, -x);
+			transform.Translate (0, y, 0);
+		}
+
+
+		
+	}
+}
