ee084bc322ed49d5ea80be58db8a412e5a65b32c
DESKTOP-HQJ4Q4R\Savion Thomas <savion1300@gmail.com>
2018-04-15 11:56:22 -0400

    render
    render image

---
+++ Assets/Scripts/Marking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marking : MonoBehaviour {
+
+    public Transform MarkPoint;
+
+	public GameObject TheMark;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	
+    
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Mark")
+		{
+			TheMark.transform.position = MarkPoint.transform.position;
+		}
+	}
+}
